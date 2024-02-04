2adf0c60a9eaf2eae8610929cb193fc7d21be82d
Aliheydar Rustamov <alihaydarustamov92@gmail.com>
2024-02-04 14:32:07 +0400

    init commit

---
+++ src/main/java/com/alinem/howtodo/HowtodoApplication.java
@@ -0,0 +1,13 @@
+package com.alinem.howtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowtodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alinem/howtodo/HowtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alinem.howtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
